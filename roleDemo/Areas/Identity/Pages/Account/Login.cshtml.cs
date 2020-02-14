582f8ce301b3ee37ec3f590a1bd390b66bd0826c
tomk <thomaskeithferris@gmail.com>
2020-02-14 13:18:19 -0800

    added security delay

--- roleDemo/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ roleDemo/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -67,6 +67,9 @@ namespace roleDemo.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            // Add five second delay
+            System.Threading.Thread.Sleep(5000); 
+
             returnUrl = returnUrl ?? Url.Content("~/");
 
             if (ModelState.IsValid)

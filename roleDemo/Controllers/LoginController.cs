2ca82cdc1cac9dd3bbf34c74c529f4648bf70a1d
tomkf <thomaskeithferris@gmail.com>
2020-02-21 10:24:48 -0800

    api controller and view model

---
+++ roleDemo/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace roleDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,
+    Roles = "Admin,Manager")]
+    public class LoginController : ControllerBase
+    {
+    }
+}
--- roleDemo/Startup.cs
+++ roleDemo/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace roleDemo {
     public class Startup {

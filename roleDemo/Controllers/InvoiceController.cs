1ab0c2e00a5353651abc16a0a438e7fa5fd39923
tomkf <thomaskeithferris@gmail.com>
2020-02-20 17:42:50 -0800

    controller and views

---
+++ roleDemo/Controllers/InvoiceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using roleDemo.Data;
+using roleDemo.Repositories;
+
+namespace roleDemo.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public InvoiceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            InvoiceRepo roleRepo = new InvoiceRepo(_context);
+            return View(roleRepo.GetAllInvoices());
+        }
+    }
+}
